c3ad701d44fa1b208f36a7a250f9994704b309c6
Sakshi Rane <sakshiramchandrarane@gmail.com>
2024-02-24 15:30:57 +0530

    update about star wars api

---
+++ star-wars-api/star-wars-api/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Main from './Components/Main';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Star Wars Planets Directory</h1>
+      </div>
+      <div>
+        <Main />
+      </div>
+    </>
+  );
+}
+
+export default App;
